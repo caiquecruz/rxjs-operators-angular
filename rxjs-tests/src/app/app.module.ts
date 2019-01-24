d0e8f1243f025c43f00c2c2bc28fffab7c474d90
Cruz, Caique <caique.cruz@dieboldnixdorf.com>
2019-01-24 16:30:18 -0200

    typeahead and changes

--- rxjs-tests/src/app/app.module.ts
+++ rxjs-tests/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { TypeaheadComponent } from './common/typeahead/typeahead.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    TypeaheadComponent
   ],
   imports: [
     BrowserModule,
