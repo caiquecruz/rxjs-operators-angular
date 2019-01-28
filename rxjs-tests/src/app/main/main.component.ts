3f01712d46b4bb04c97d1edd9b8fc27835c2fdd3
Cruz, Caique <caique.cruz@dieboldnixdorf.com>
2019-01-28 15:28:47 -0200

    added rxjs operators

--- rxjs-tests/src/app/main/main.component.ts
+++ rxjs-tests/src/app/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import { find, filter, merge, delay, reduce, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  myArray: Array<any> = [1, 2, 3, 4, 'Teste 1', 40, 20, 'Teste 2', 8, 9, 'Teste 3'];
+  myExtraArray: Array<any> = [10, 30, 50, 70, 10, 20, 'Teste 4', 'Teste 5'];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getIndex() {
+    from(this.myArray).pipe(
+      find((i) => i > 10)
+    )
+    .subscribe((index) => {
+      alert(index);
+    });
+  }
+
+  doFilter() {
+    from(this.myArray).pipe(
+      filter((i) => typeof i === 'string')
+    )
+    .subscribe((index) => {
+      alert(index);
+    });
+  }
+
+  doMerge() {
+    from(this.myArray).pipe(
+      delay(1000),
+      merge(this.myExtraArray)
+    )
+    .subscribe((newArray) => {
+      console.log(newArray);
+    });
+  }
+
+  doFilterAndReduce() {
+    from(this.myArray).pipe(
+      merge(this.myExtraArray),
+      filter((i) => typeof i === 'number'),
+      reduce((acc, i) => acc + i)
+    )
+    .subscribe((reduced) => {
+      alert(reduced);
+    });
+  }
+
+  doTupleMap() {
+    from(this.myArray).pipe(
+      filter((i) => typeof i === 'number'),
+      map(items => {
+        return [items * 2, items / 2] as [Number, Number];
+      })
+    )
+    .subscribe((itens) => {
+      console.log(itens);
+    });
   }
 
 }
